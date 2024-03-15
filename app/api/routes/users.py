d3ced316ade6a0850d26840f9797c808f08a558e
DanteDeLordran <dantelopez305@outlook.com>
2024-03-14 22:19:25 -0600

    Method for editin phone number

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -12,10 +12,18 @@ async def get_user( db : db_dependency , user : user_dependency ):
 
 @user_router.put('/edit/password')
 async def change_user_password( db : db_dependency , user : user_dependency , request : PasswordChangeRequest):
-    user = db.query(User).filter(User._id == user.get('user_id')).first()
+    found_user = db.query(User).filter(User._id == user.get('user_id')).first()
     if not bcrypt_context.verify( request.password , user.password ):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    user.password = bcrypt_context.hash(request.new_password)
-    db.add(user)
+    found_user.password = bcrypt_context.hash(request.new_password)
+    db.add(found_user)
     db.commit()
     return 'Successfully edited password'
+
+@user_router.put('/edit/phonenumber')
+async def update_user_phone_number( db : db_dependency, user : user_dependency , phone_number = Query(min_length=9) ):
+    found_user = db.query(User).filter(User._id == user.get('user_id')).first()
+    found_user.phone_number = phone_number
+    db.add(found_user)
+    db.commit()
+    return f'Phone number {phone_number} successfully edited'
