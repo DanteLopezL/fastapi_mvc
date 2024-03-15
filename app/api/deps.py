4aa012000f550ba0da6a8730fef09c7a321adc68
DanteDeLordran <dantelopez305@outlook.com>
2024-03-14 19:24:26 -0600

    Now deps is the module in charge of getting the current user in the context

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,5 +1,7 @@
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, HTTPException , status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError , jwt
 from app.db.database import SessionLocal
 from sqlalchemy.orm import Session
 
@@ -9,6 +11,29 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='/auth/token')
+
+SECRET_KEY = 'bG9yZHJhbmRhcnNhcmFz'
+ALGORITHM = 'HS256'
+
+async def get_current_user(token : Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username : str = payload.get('sub')
+        user_id : int = payload.get('id')
+        role : str = payload.get('role')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid credentials')
+        return {
+            'username' : username,
+            'user_id': user_id,
+            'role' : role
+        }
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid credentials')
 
 # Database dependency injection for query methods
 db_dependency = Annotated[Session, Depends(get_db)]
+
+user_dependency = Annotated[dict , Depends(get_current_user)]
