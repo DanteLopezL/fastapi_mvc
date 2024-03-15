9eadaa83643cd714e83c3564d7c51a6f3ae6fd8e
DanteDeLordran <dantelopez305@outlook.com>
2024-03-14 20:11:15 -0600

    Fixed error in role comparition

--- app/api/routes/admin.py
+++ app/api/routes/admin.py
@@ -6,6 +6,6 @@ admin_router = APIRouter()
 
 @admin_router.get('/todos/all')
 async def get_all_todos( db : db_dependency , user : user_dependency ):
-    if user.get('role') is not 'admin':
+    if user.get('role') != 'admin':
         raise HTTPException(status_code=401, detail='Invalid cedentials')
     return db.query(Todo).all()
