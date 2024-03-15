c961c141aa55f9e8b3050b6537449295f4e11f2c
DanteDeLordran <dantelopez305@outlook.com>
2024-03-14 22:47:04 -0600

    Added env vars

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://lordran_dev:lordran_dev@localhost:5433/fastapi_db')
+# Retrieve environment variables
+db_user = os.getenv('DB_USER', 'lordran_dev')
+db_password = os.getenv('DB_PASSWORD', 'lordran_dev')
+db_host = os.getenv('DB_HOST', 'localhost')
+db_port = os.getenv('DB_PORT', '5433')
+db_name = os.getenv('DB_NAME', 'fastapi_db')
 
+# Create database connection URL
+db_url = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
+# Create SQLAlchemy engine
+engine = create_engine(db_url)
+
+# Create a sessionmaker
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
+# Create a base class for declarative data models
 Base = declarative_base()
-
