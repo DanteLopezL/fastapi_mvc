08e829c93142255285f15ffcf204c7b6e645cf37
DanteDeLordran <lordran@lordran.dev>
2024-03-17 20:44:04 -0600

    Added env variables

--- app/db/database.py
+++ app/db/database.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Retrieve environment variables
-db_user = os.getenv('DB_USER', 'lordran_dev')
-db_password = os.getenv('DB_PASSWORD', 'lordran_dev')
-db_host = os.getenv('DB_HOST', 'localhost')
-db_port = os.getenv('DB_PORT', '5433')
-db_name = os.getenv('DB_NAME', 'fastapi_db')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
 
 # Create database connection URL
 db_url = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
